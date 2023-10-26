ea012bce55ce225b652297dec8d4f1795cf246b3
Breno Ferraz Ferreira <81932347+BrenoFerrazFerreira@users.noreply.github.com>
2023-10-26 15:00:01 -0300

    Add files via upload

---
+++ prjExemplo_OO_BD_Parte_3/src/Cliente.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * @author Dimas
+ */
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private double limCred;
+    private double limCredDisp;
+
+    public Cliente(String c, String n, double lc) {
+        cpf = c;
+        nome = n;
+        limCred = lc;
+        limCredDisp = limCred;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getLimCred() {
+        return limCred;
+    }
+
+    public double getLimCredDisp() {
+        return limCredDisp;
+    }
+
+    public void setNome(String n) {
+        nome = n;
+    }
+
+    public void setLimCred(double lc) {
+        limCred = lc;
+    }
+
+    public void setLimCredDisp(double lcd) {
+        limCredDisp = lcd;
+    }
+    
+    
+}
